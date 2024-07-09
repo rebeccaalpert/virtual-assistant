462c190e5b15590af094e4ebdef8ba57bc6eda56
nicolethoen <nthoen@redhat.com>
2024-07-09 15:30:33 -0400

    fix: update e2e test urls

--- cypress/e2e/VirtualAssistant.spec.cy.ts
+++ cypress/e2e/VirtualAssistant.spec.cy.ts
@@ -1,7 +1,7 @@
 describe('Test the Virtual assistant docs page', () => {
 
   it('renders listening to messages', () => {
-    cy.visit('http://localhost:8006/extensions/virtual-assistant/virtual-assistant');
+    cy.visit('http://localhost:8006/extensions/chat-bots--ai/virtual-assistant');
     cy.wait(1000);
 
     cy.get('[data-test-id="assistant-example-message"]').should('contain', 'Last received message: ');
@@ -11,7 +11,7 @@ describe('Test the Virtual assistant docs page', () => {
   })
 
   it('renders header with actions', () => {
-    cy.visit('http://localhost:8006/extensions/virtual-assistant/virtual-assistant', { onBeforeLoad: (win) => {cy.stub(win.console, 'log').as('consoleLog');} });
+    cy.visit('http://localhost:8006/extensions/chat-bots--ai/virtual-assistant', { onBeforeLoad: (win) => {cy.stub(win.console, 'log').as('consoleLog');} });
     cy.wait(1000);
     cy.get('[aria-label="Minimize virtual assistant"]').click({ force: true });
     cy.wait(1000);
