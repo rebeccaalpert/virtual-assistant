5910a40752bfc22bf173799f14b56a4899e2fe76
Rebecca Alpert <ralpert@redhat.com>
2024-09-04 14:05:58 -0400

    fix(FileDetailsLabel): Adjust styles for clickable button
    Switch to clickable label meant that the text alignment was off. This should fix it.

--- packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/FileDetailsLabel/FileDetailsLabel.tsx
+++ packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/FileDetailsLabel/FileDetailsLabel.tsx
@@ -3,6 +3,7 @@ import FileDetailsLabel from '@patternfly/virtual-assistant/dist/dynamic/FileDet
 
 export const BasicDemo: React.FunctionComponent = () => (
   <div className="pf-chatbot__file-details-example">
-    <FileDetailsLabel fileName="auth-operator" language="YAML" onClose={() => undefined} />
+    {/* eslint-disable-next-line no-console */}
+    <FileDetailsLabel fileName="auth-operator" language="YAML" onClose={() => console.log('clicked close button!')} />
   </div>
 );
