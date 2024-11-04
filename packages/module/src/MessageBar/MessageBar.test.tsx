1a2445afd614894737f8f561e0f7b6c00b73d8f2
Rebecca Alpert <ralpert@redhat.com>
2024-11-04 17:35:21 -0500

    chore(MessageBar): Add tests for MessageBar

---
+++ packages/module/src/MessageBar/MessageBar.test.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { MessageBar } from './MessageBar';
+import { DropdownGroup, DropdownItem, DropdownList } from '@patternfly/react-core';
+import SourceDetailsMenuItem from '../SourceDetailsMenuItem';
+import { BellIcon, CalendarAltIcon, ClipboardIcon, CodeIcon } from '@patternfly/react-icons';
+
+const ATTACH_MENU_ITEMS = [
+  <DropdownList key="list-1">
+    <DropdownItem className="pf-chatbot-source-details-dropdown-item" value="auth-operator Pod" id="0">
+      <SourceDetailsMenuItem
+        icon={
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0 12.5C0 5.87258 5.37258 0.5 12 0.5C18.6274 0.5 24 5.87258 24 12.5C24 19.1274 18.6274 24.5 12 24.5C5.37258 24.5 0 19.1274 0 12.5Z"
+              fill="currentColor"
+            />
+            <g clipPath="url(#clip0_3280_27488)">
+              <path
+                d="M8.25 8.75C8.25 7.92266 8.92266 7.25 9.75 7.25H12C14.0719 7.25 15.75 8.92812 15.75 11C15.75 13.0719 14.0719 14.75 12 14.75H9.75V17C9.75 17.4148 9.41484 17.75 9 17.75C8.58516 17.75 8.25 17.4148 8.25 17V14V8.75ZM9.75 13.25H12C13.2422 13.25 14.25 12.2422 14.25 11C14.25 9.75781 13.2422 8.75 12 8.75H9.75V13.25Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_3280_27488">
+                <rect width="7.5" height="12" fill="white" transform="translate(8.25 6.5)" />
+              </clipPath>
+            </defs>
+          </svg>
+        }
+        name="auth-operator"
+        type="Pod"
+      />
+    </DropdownItem>
+  </DropdownList>,
+  <DropdownGroup key="group2">
+    <DropdownList>
+      <DropdownItem value="Alerts" id="1" icon={<BellIcon />}>
+        Alerts
+      </DropdownItem>
+      <DropdownItem value="Events" id="2" icon={<CalendarAltIcon />}>
+        Events
+      </DropdownItem>
+      <DropdownItem value="Logs" id="3" icon={<ClipboardIcon />}>
+        Logs
+      </DropdownItem>
+      <DropdownItem value="YAML - Status" id="4" icon={<CodeIcon />}>
+        YAML - Status
+      </DropdownItem>
+      <DropdownItem value="YAML - All contents" id="5" icon={<CodeIcon />}>
+        YAML - All contents
+      </DropdownItem>
+    </DropdownList>
+  </DropdownGroup>
+];
+
+describe('Message bar', () => {
+  it('should render correctly', () => {
+    render(<MessageBar onSendMessage={jest.fn} />);
+    expect(screen.getByRole('button', { name: 'Attach button' })).toBeTruthy();
+    expect(screen.queryByRole('button', { name: 'Send button' })).toBeFalsy();
+    // jest doesn't have browser recognition
+    expect(screen.queryByRole('button', { name: 'Microphone button' })).toBeFalsy();
+    expect(screen.getByRole('textbox', { name: /Send a message.../i })).toBeTruthy();
+  });
+  it('shows send button when text is input', async () => {
+    render(<MessageBar onSendMessage={jest.fn} />);
+    const input = screen.getByRole('textbox', { name: /Send a message.../i });
+    await userEvent.type(input, 'Hello world');
+    expect(input).toHaveValue('Hello world');
+    expect(screen.getByRole('button', { name: 'Send button' })).toBeTruthy();
+  });
+  it('can click send button', async () => {
+    const spy = jest.fn();
+    render(<MessageBar onSendMessage={spy} />);
+    const input = screen.getByRole('textbox', { name: /Send a message.../i });
+    await userEvent.type(input, 'Hello world');
+    expect(input).toHaveValue('Hello world');
+    const sendButton = screen.getByRole('button', { name: 'Send button' });
+    expect(sendButton).toBeTruthy();
+    await userEvent.click(sendButton);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  it('can send via enter key', async () => {
+    const spy = jest.fn();
+    render(<MessageBar onSendMessage={spy} />);
+    const input = screen.getByRole('textbox', { name: /Send a message.../i });
+    await userEvent.type(input, 'Hello world');
+    expect(input).toHaveValue('Hello world');
+    await userEvent.type(input, '[Enter]');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  it('can show attach menu', async () => {
+    render(
+      <MessageBar
+        onSendMessage={jest.fn}
+        attachMenuProps={{
+          isAttachMenuOpen: true,
+          setIsAttachMenuOpen: jest.fn(),
+          onAttachMenuToggleClick: jest.fn(),
+          onAttachMenuInputChange: jest.fn(),
+          attachMenuItems: ATTACH_MENU_ITEMS
+        }}
+      />
+    );
+    expect(screen.getByRole('textbox', { name: /Filter menu items/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /auth-operator/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /Alerts/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /Events/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /Logs/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /YAML - Status/i })).toBeTruthy();
+    expect(screen.getByRole('menuitem', { name: /YAML - All contents/i })).toBeTruthy();
+  });
+  it('can toggle attach menu', async () => {
+    const attachToggleClickSpy = jest.fn();
+    render(
+      <MessageBar
+        onSendMessage={jest.fn}
+        attachMenuProps={{
+          isAttachMenuOpen: false,
+          setIsAttachMenuOpen: jest.fn(),
+          onAttachMenuToggleClick: attachToggleClickSpy,
+          onAttachMenuInputChange: jest.fn(),
+          attachMenuItems: ATTACH_MENU_ITEMS
+        }}
+      />
+    );
+    expect(screen.queryByRole('textbox', { name: /Filter menu items/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /auth-operator/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /Alerts/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /Events/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /Logs/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /YAML - Status/i })).toBeFalsy();
+    expect(screen.queryByRole('menuitem', { name: /YAML - All contents/i })).toBeFalsy();
+    const attachButton = screen.getByRole('button', { name: 'Attach button' });
+    await userEvent.click(attachButton);
+    expect(attachToggleClickSpy).toHaveBeenCalledTimes(1);
+  });
+  it('can show stop button', () => {
+    render(<MessageBar onSendMessage={jest.fn} hasStopButton handleStopButton={jest.fn} />);
+    expect(screen.getByRole('button', { name: 'Stop button' })).toBeTruthy();
+  });
+});
