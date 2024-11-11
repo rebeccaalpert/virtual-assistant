42ed0257f01c79e14b1bf9d807c7e6e62b0f5773
Rebecca Alpert <ralpert@redhat.com>
2024-11-11 18:57:04 -0500

    chore(docs): Address feedback
    Also fixing test with build error after rebase.

--- packages/module/src/ResponseActions/ResponseActions.test.tsx
+++ packages/module/src/ResponseActions/ResponseActions.test.tsx
@@ -31,11 +31,11 @@ describe('ResponseActions', () => {
 
   it('should be able to change aria labels', () => {
     const actions = [
-      { type: 'positive', ariaLabel: /Thumbs up/i },
-      { type: 'negative', ariaLabel: /Thumbs down/i },
-      { type: 'copy', ariaLabel: /Copy the message/i },
-      { type: 'share', ariaLabel: /Share it with friends/i },
-      { type: 'listen', ariaLabel: /Listen up/i }
+      { type: 'positive', ariaLabel: 'Thumbs up' },
+      { type: 'negative', ariaLabel: 'Thumbs down' },
+      { type: 'copy', ariaLabel: 'Copy the message' },
+      { type: 'share', ariaLabel: 'Share it with friends' },
+      { type: 'listen', ariaLabel: 'Listen up' }
     ];
     actions.forEach(({ type, ariaLabel }) => {
       render(<ResponseActions actions={{ [type]: { onClick: jest.fn(), ariaLabel } }} />);
