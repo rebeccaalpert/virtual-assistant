bf2454b7fa064acfb180502eecbada3e92fb77bf
Rebecca Alpert <ralpert@redhat.com>
2025-04-29 14:17:17 -0400

    fix(JumpLinks): Add onClick handlers
    Expose onClick handlers so developers can further customize scroll behavior.

--- packages/module/src/MessageBox/MessageBox.test.tsx
+++ packages/module/src/MessageBox/MessageBox.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MessageBox } from './MessageBox';
+import userEvent from '@testing-library/user-event';
 
 describe('MessageBox', () => {
   it('should render Message box', () => {
@@ -22,5 +23,48 @@ describe('MessageBox', () => {
 
     expect(ref.current).not.toBeNull();
     expect(ref.current).toBeInstanceOf(HTMLDivElement);
+  });
+  it('should call onScrollToBottomClick when scroll to top button is clicked', async () => {
+    const spy = jest.fn();
+    render(
+      <MessageBox onScrollToBottomClick={spy}>
+        <div>Test message content</div>
+      </MessageBox>
+    );
+
+    // this forces button to show
+    const region = screen.getByRole('region');
+    Object.defineProperty(region, 'scrollHeight', { configurable: true, value: 1000 });
+    Object.defineProperty(region, 'clientHeight', { configurable: true, value: 500 });
+    Object.defineProperty(region, 'scrollTop', { configurable: true, value: 0 });
+    region.dispatchEvent(new Event('scroll'));
+
+    await waitFor(() => {
+      userEvent.click(screen.getByRole('button', { name: /Jump bottom button/i }));
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+  it('should call onScrollToTopClick when scroll to top button is clicked', async () => {
+    const spy = jest.fn();
+    render(
+      <MessageBox onScrollToTopClick={spy}>
+        <div>Test message content</div>
+      </MessageBox>
+    );
+
+    // this forces button to show
+    const region = screen.getByRole('region');
+    Object.defineProperty(region, 'scrollHeight', { configurable: true, value: 1000 });
+    Object.defineProperty(region, 'clientHeight', { configurable: true, value: 500 });
+    Object.defineProperty(region, 'scrollTop', {
+      configurable: true,
+      value: 500
+    });
+    region.dispatchEvent(new Event('scroll'));
+
+    await waitFor(() => {
+      userEvent.click(screen.getByRole('button', { name: /Jump top button/i }));
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
